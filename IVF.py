0f562c492cc093a10454824da59207c0e18c4f22
kikohafez78 <76871285+kikohafez78@users.noreply.github.com>
2023-12-13 03:33:17 +0200

    updated IVF.py

--- IVF.py
+++ IVF.py
@@ -1,7 +1,16 @@
 import numpy as np
 from scipy.cluster.vq import kmeans2
+from sklearn.metrics.pairwise import cosine_similarity
+from operator import itemgetter
 
 
+def sort_list(list1, list2):
+ 
+    zipped_pairs = zip(list2, list1)
+ 
+    z = [x for _, x in sorted(zipped_pairs)]
+ 
+    return z
 
 class IVFile(object):
     
@@ -19,7 +28,6 @@ class IVFile(object):
         x = 0
         for k in index:
             byte_file = np.asarray(k)
-            print(centroids[x])
             centroid_assignment[str(centroids[x])] = f"./data/file{x}.npy"
             file = open(f"./data/file{x}.npy",'a')
             np.save(f"./data/file{x}.npy", byte_file)
@@ -27,7 +35,30 @@ class IVFile(object):
             x += 1
             k = None
         self.assigments =  centroid_assignment
-        return self.assigments 
+        return self.assigments
     
-    def get_cluster_data(centroid: np.ndarray):
-        
+    def get_closest_centroids(self, vector: np.ndarray, K: int):
+        centroids = self.data[0]
+        distances = np.absolute(cosine_similarity(vector,centroids))
+        return sort_list(centroids,distances)
+    
+    def get_cluster_data(self, centroid: np.ndarray, vector: np.ndarray, K: int):
+        data = np.load(self.assigments[str(centroid)])
+        distances = cosine_similarity(vector,data)
+        indices = np.argpartition(distances,K)[:K]
+        sorted_indices = indices[np.argsort(data[indices])]
+        return data[sorted_indices]
+    
+# K = 3
+# num_partions = 16
+# dataset = np.random.normal(size = (1000,10))
+# Iv = IVFile(num_partions, dataset)
+# a = Iv.clustering()
+# test_vector = np.random.normal(size= (1,10))
+# #===========================================================================
+# print("test-vector:",test_vector)
+# #===========================================================================
+# # print("centroid to file: ",a,"\n")
+# #===========================================================================
+# closest= Iv.get_closest_centroids(test_vector, K)
+# print(f"{K} closest clusters are: ",closest)
