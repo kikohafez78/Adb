a850d5bae08cdc36cceb94a65d865a78ec689fb6
Omar Badr <omarbadrh.ob@gmail.com>
2023-12-13 05:53:29 +0200

    search_layer function is intially done, Not tested

--- hsnw.py
+++ hsnw.py
@@ -11,8 +11,10 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
 import threading
 import math
+
 # import numba as nm
 import random
+from heapq import *
 
 dimension = 70
 segments = 14
@@ -29,11 +31,18 @@ class Node(object):
         self.M = M
         self.layers = all_layers
         self.M_MAX = M_MAX
-        self.friends_list = []
+        self.friends_list = []  # list of tuples (cosine_similarity, node)
 
     # @nm.set(fast_math = True)
     def get_distance_n_similarity(self, vector: np.ndarray):
-        return cosine_distances(self.vec, vector), cosine_similarity(self.vec, vector)
+        return cosine_similarity(self.vec, vector)
+
+
+# functions for creating a heap that are sorted by cosine similarity between elements and query vector
+def sorted_list_by_cosine_similarity(heap: list, query_vector: np.ndarray):
+    heap = [(cosine_similarity(node.vec, query_vector), node) for node in heap]
+    heap.sort(reverse=True)  # sort descending
+    return heap
 
 
 # good values for M lie between 5 and 48, High M is better for high dimensionality and recall -> dim = 70 therefore M must be High
@@ -42,7 +51,7 @@ class Node(object):
 # heuristic of selecting M out of efConstruction is both closest Nodes and connectivity of th graph by considering the connectivity distances between the candidates
 class vector_db(object):
     def __init__(
-        self, M: int ,efSearch: int, efConstruction
+        self, M: int, efSearch: int, efConstruction
     ):  # M is max number of links per node, M_MAX is max number of links connected to the node, efSearch is number of threads or number of nearest neighbors to use to find closest query result
         # ml 0-1 is the overlap between layers (recommended 1/ln(M)
         self.M = M
@@ -52,8 +61,7 @@ class vector_db(object):
         self.Layers = -1
         self.max_layers = 0
         self.ml = 1.0 / math.log(M)
-        self.graph = [[]]  # graph is a list of lists of nodes
-        
+        self.graph = np.empty((0, 0), dtype=Node)
 
     """
     SEARCH-LAYER(q, ep, ef, lc)
@@ -79,13 +87,65 @@ class vector_db(object):
         18 return W 
     """
 
-    def search_layer(query_element, entry_points, ef_search, layer):
-        pass
+    def search_layer(
+        query_element: np.ndarray,
+        entry_points: list[(int, Node)],
+        ef_search: int,
+        layer: int,
+    ):
+        entry_points = sorted_list_by_cosine_similarity(
+            entry_points, query_element
+        )  # sort entry points by cosine similarity
+        v = entry_points  # visited elements
+        C = entry_points  # candidates
+        W = entry_points  # dynamic list of found nearest neighbors
+        # while I have candidates
+        while len(C) > 0:
+            # extract nearest element from C to q
+            c = entry_points.pop(0)
+            # get furthest element from W to q
+            f = W[-1]
+            # if distance between c and q is greater than distance between f and q then you are done
+            if c[0] > f[0]:
+                break
+            # iterate over friend list of c, where you will update candidates and results accordingly.
+            for e in c[1].friends_list:
+                if e not in v:
+                    v.append(e)
+                    v.sort(reversed=True)
+                    f = W[-1]
+                    if e[0] < f[0] or len(W) < ef_search:
+                        C.append(e)
+                        W.append(e)
+                        C.sort(reversed=True)
+                        W.sort(reversed=True)
+                        if len(W) > ef_search:
+                            W.pop()
+        return W
 
-    def select_neighbors_simple(self, query_element, ef_search, layer):
-        pass
+    def select_neighbors_simple(
+        self, query_element: np.ndarray, C: list[(int, Node)], M: int
+    ):
+        return sorted_list_by_cosine_similarity(C, query_element)[0:M]
 
-    def select_neighbors_heuristic(self, query_element, ef_search, layer):
+    """
+    SELECT-NEIGHBORS-HEURISTIC(q, C, M, lc, extendCandidates, keepPrunedConnections)
+        Input: base element q, candidate elements C, number of neighbors to
+        return M, layer number lc, flag indicating whether or not to extend
+        candidate list extendCandidates, flag indicating whether or not to add
+        discarded elements keepPrunedConnections
+
+    """
+
+    def select_neighbors_heuristic(
+        self,
+        query_element: np.ndarray,
+        C : list[(int, Node)],
+        M: int,
+        layer: int,
+        extendCandidates=False,
+        keepPrunedConnections=False,
+    ):
         pass
 
     def select_neighbors_knn(self, query_element, ef_search, layer):
